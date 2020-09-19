1b8cc9f235d3fcfe4fee4e8f2d1d5637b55f236c
Unknown <unknown@example.com>
2020-09-19 16:10:38 +0100

    finish coding the layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,5 +2,8 @@ const conatiner = document.querySelector('.container');
 const square1 = document.getElementById('square1');
 const square2 = document.getElementById('square2');
 
-Draggable.create('#square1');
+Draggable.create('#square1',{
+    bounds:conatiner
+});
 Draggable.create('#square2');
+
