c6487005a2a3e8f37acd7455dc386bdef14ce5ce
Unknown <unknown@example.com>
2020-09-19 14:25:02 +0100

    inniitail commit

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+const conatiner = document.querySelector('.container');
+const square1 = document.getElementById('square1');
+const square2 = document.getElementById('square2');
+
+Draggable.create('#square1');
+Draggable.create('#square2');
